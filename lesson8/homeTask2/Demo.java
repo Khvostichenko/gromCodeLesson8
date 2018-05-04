a70f98ebad446a1994d260c0cb3ebf6e41a620b4
oleg <oleg.khvostichenko@gmail.com>
2018-05-04 10:14:05 +0300

    Right variant of program.

--- lesson8/homeTask2/Demo.java
+++ lesson8/homeTask2/Demo.java
@@ -1,14 +1,17 @@
 package lesson8.homeTask2;
 
 public class Demo {
+
+    Course[] courses = {};
+
     public Student createHighestParent(){
-        Student student = new Student("Stive", "Jobs",5,null);
+        Student student = new Student("Stive", "Jobs",5,courses);
 
         return student;
     }
 
     public SpecialStudent createLowestChild(){
-        SpecialStudent specialStudent = new SpecialStudent("Stive","Vozniak",10,null,34543543,"wonderfulStive");
+        SpecialStudent specialStudent = new SpecialStudent("Stive","Vozniak",10,courses,34543543,"wonderfulStive");
 
         return specialStudent;
     }
